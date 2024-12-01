354f969e28d11361168411d7135197b666846efc
ygvbhy <ygvbhy@naver.com>
2024-12-01 17:53:46 +0900

    틱택토 게임 첫 단계 시작 - 헤더 생성 및 플레이어 창

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <h1></h1>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
 export default App;
