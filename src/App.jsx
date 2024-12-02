b1af3493198d30cefc0af1eabe1d45b0c9db681d
ygvbhy <ygvbhy@naver.com>
2024-12-02 21:10:09 +0900

    분리된 파일로 데이터 아웃소싱

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combination";
 
 const deriveActivePlayer = (gameTurns) => {
   let currentPlayer = "X";
